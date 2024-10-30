5ba8bedeeddd4a130bc8d7cb4a80093931543b96
Edmilson de Azevedo Cassecasse <55765693+eacassecasse@users.noreply.github.com>
2024-10-30 16:48:33 +0000

    refactor: Using Ordered Dictionary instead of calculating the ranks manual

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -15,13 +15,15 @@ class LRUCache(BaseCaching):
 
     def put(self, key, item):
         """ Assigns a value to a cache item using LRU. """
-        if key and item:
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
 
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                discarded, _ = self.cache_data.popitem(last=False)
-                print("DISCARD:", discarded)
-            self.cache_data.move_to_end(key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discarded, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {discarded}")
 
     def get(self, key):
         """ Retrieves a cached item. """
